02960a41bcea1b78b35cafdb092eabfe7bf6efbf
Nadine Gutiérrez <36292059+Nadine044@users.noreply.github.com>
2021-12-20 14:37:45 +0100

    Save&Load WIP

---
+++ Heatmap_VisualDataAnalysis/Assets/3DGamekitLite/HeatmapScripts/load_data.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class load_data : MonoBehaviour
+{
+    /*public data_file daton;
+    public KPIs data;
+
+    public struct KPIs
+    {
+        public List<Vector3> kills_pos; //number of killed monsters
+        public List<Vector3> hits_pos;
+        public List<Vector3> lost_acid_pos;
+        public List<Vector3> deaths_pos; //number of times that Ellen dies
+    }
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Debug.Log(data.kills_pos[0]);
+            Debug.Log(data.hits_pos[0]);
+            Debug.Log(data.lost_acid_pos[0]);
+            Debug.Log(data.deaths_pos[0]);
+        }
+    }
+
+    public void AssignData()
+    {
+        data.kills_pos = daton.data_loaded.kills_pos;
+    }*/
+}
