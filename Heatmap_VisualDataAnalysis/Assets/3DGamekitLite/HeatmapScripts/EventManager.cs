a83ffd1253f71e8d7476dfd76d1cf68796a2dd80
Jorge <jgherencia@gmail.com>
2022-01-07 21:53:35 +0100

    Saving death, hitted, and kill positions
    Remain path and acid

--- Heatmap_VisualDataAnalysis/Assets/3DGamekitLite/HeatmapScripts/EventManager.cs
+++ Heatmap_VisualDataAnalysis/Assets/3DGamekitLite/HeatmapScripts/EventManager.cs
@@ -11,16 +11,34 @@ public class EventManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //subscribe to Ellen "death" and "hit" events.
         ellen.OnDeath.AddListener(SaveDeathData);
-    }
+        ellen.OnReceiveDamage.AddListener(SaveHitData);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        //subscribe to enemies "death" event wich will be the kill position of ellen.
+        Damageable[] AllObjects = FindObjectsOfType<Damageable>();
+        foreach (Damageable obj in AllObjects)
+        {
+            if (obj.gameObject.layer == 23/*Enemies*/ && obj.gameObject.name != "Cube")
+            {
+                obj.OnDeath.AddListener(SaveKillData);
+                Debug.Log(obj.gameObject.name);
+                Debug.Log(obj.gameObject.transform.position);
+
+            }
+        }
     }
     void SaveDeathData()
     {
         data.all_data.death_pos.Add(ellen.transform.position);
+    }
+
+    void SaveHitData()
+    {
+        data.all_data.hit_pos.Add(ellen.transform.position);
+    }
+    void SaveKillData()
+    {
+        data.all_data.kill_pos.Add(ellen.transform.position); //Pos of Ellen killing enemies
     }
 }
